4821b4350bf0f084994c2aeb1f59c0840a67b989
NestJul <nestjul@yandex.ru>
2025-05-07 00:16:23 +0700

    add task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((element, index) => {
+
+		return element === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if (!users || users.length === 0) {
+		return 0;
+	}
+	const filteredUsers = users.filter(user => user.gender === gender);
+	const ages = filteredUsers.map(user => user.age);
+	if (ages.length === 0) {
+		return 0;
+	}
+
+	const averageAge = ages.reduce((sum, age) => sum + age, 0) / ages.length;
+
+	return averageAge;
 }
