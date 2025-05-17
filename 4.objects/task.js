05c5e0fbb86b3f4e4b1666f80f2053c8f2c402dc
NestJul <nestjul@yandex.ru>
2025-05-17 22:26:30 +0700

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
 
-Student.prototype.getAverage = function () {
-  
-}
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+
+	if (this.marks && !this.excluded) {
+		this.marks = this.marks.concat(marksToAdd);
+	}
+};
+
+
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+
+	const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+	return sum / this.marks.length;
+};
+
+
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+};
